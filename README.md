b6afb7ed923df5766fe07790493909f6d01be143
Pablo Balbi <pablo.l.balbi@gmail.com>
2021-06-12 18:12:19 -0300

    Fixed README. Added library under test discovery

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Local docker container tag being used: `b5d5ab012d81`
 2. Start the Typescript compiler in watch mode: ```npm install && npx tsc --watch```
 3. Given the built container hash, start it mounting the built analysis directory:
 ```bash
-docker run -v `pwd`/dist/:/analysis -ti $CONTAINER_TAG  bash
+docker run -v `pwd`/:/analysis -ti $CONTAINER_TAG  bash
 ```
 4. Inside the container, run the analysis with:
 ```bash
@@ -15,6 +15,6 @@ cd graalvm-nodeprof-java8-20.2.0-dev
 ./bin/node \
     --experimental-options \
     --nodeprof ./nodeprof/nodeprof.js \
-    --analysis  /analysis/index.js \
+    --analysis  /analysis/dist/index.js \
         ./nodeprof/tests/helloworld.js
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,49 @@
 import {JalangiAnalysis} from "./jalangi";
 import {Jalangi} from "./types/Jalanig";
 import {NamedFunction} from "./types/Extensions";
+import {debug as _debug} from "debug";
 
+const debug = _debug("slim_taser");
 declare var J$: Jalangi;
 
 // DO NOT INSTRUMENT
 class SomeAnalysis extends JalangiAnalysis {
+    private readonly libraryUnderTest: string;
+    private entryPoints: NamedFunction[] = [];
+
+    constructor() {
+        super();
+        const lut = process.env.LIBRARY_UNDER_TEST;
+        if (lut == undefined) {
+            throw new Error("LIBRARY_UNDER_TEST envvar not defined!");
+        }
+        this.libraryUnderTest = lut;
+        //@ts-ignore
+        let actualLut = require(lut);
+        if (typeof actualLut === "function") {
+            // The module just exports a function. Sth like `module.exports = function( ...`
+            this.entryPoints = [actualLut as NamedFunction];
+            debug("Library under test [%s] just exports one function named [%s]", this.libraryUnderTest, this.entryPoints[0].name);
+        } else {
+            // Module must export several things
+            for (let [key, value] of Object.entries(actualLut)) {
+                if (typeof value === "function") {
+                    let valueAsNF = value as NamedFunction;
+                    this.entryPoints.push(valueAsNF);
+                    debug("Discovered entrypoint called [%s]", valueAsNF.name);
+                }
+            }
+        }
+
+    }
+
     invokeFunPre(iid: number, f: NamedFunction, base: Object, args: Object[], isConstructor: boolean, isMethod: boolean, functionIid: number, functionSid: number): { f: NamedFunction; base: object; args: Array<any>; skip: boolean } | undefined {
         console.log("Called function named: %s", f.name);
         return super.invokeFunPre(iid, f, base, args, isConstructor, isMethod, functionIid, functionSid);
+    }
+
+    endExecution(): void {
+        console.log("Discovered entrypoints: %s", this.entryPoints.map(ep => ep.name).join(","));
     }
 }
 
