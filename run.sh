aa659642f4f74aff10b1308b3b21dfbad4a4e573
Pablo Balbi <pablo.l.balbi@gmail.com>
2021-06-12 22:07:25 -0300

    Fixed run

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 $PATH_GRAALVM/bin/node --jvm --experimental-options \
   --vm.Dtruffle.class.path.append=$PATH_NODEPROF_JAR/nodeprof.jar \
   --nodeprof $PATH_NODEPROF/src/ch.usi.inf.nodeprof/js/jalangi.js \
-  --analysis /persistent2/tsm-setup/taser/dist/src/AintNodeTaint.js $@
+  --analysis /analysis/dist/index.js $@
+
